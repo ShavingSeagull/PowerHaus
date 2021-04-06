337bc65e169628fb1082dfde4f911a6789bc2c60
Scott Kipp <scott.ecfx@gmail.com>
2021-04-06 12:39:46 +0100

    Added previously made JS files for navbar and home page

---
+++ home/static/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	// Remove the last three featured products on phone screens to prevent the 
+	// user having to scroll for a long period to reach the end of the page
+	function addRemoveProducts() {
+		const FEATUREDPRODUCTS = document.getElementsByClassName('featured-product');
+
+		if ($(window).innerWidth() <= 576) {
+			// Only removes products 3-6, leaving three on-screen
+			for (let i = 3; i < FEATUREDPRODUCTS.length; i++) {
+				FEATUREDPRODUCTS.item(i).classList.add('d-none');
+			}
+		} else {
+			$('.featured-product').removeClass('d-none');
+		}
+	}
+
+	// Call the function on page load
+	addRemoveProducts();
+
+	// A safeguard to keep the correct number of products on-screen if the 
+	// user resizes their browser window
+	$(window).resize(() => {
+		addRemoveProducts();
+	});
+});
