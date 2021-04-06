a6999a4a4e2a425621b7a08d248eee38a2b17d80
Scott Kipp <scott.ecfx@gmail.com>
2021-04-06 18:11:00 +0100

    Removed old way of linking the BASE_DIR

--- powerhaus/settings.py
+++ powerhaus/settings.py
@@ -17,7 +17,6 @@ if os.path.exists("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -82,7 +81,6 @@ WSGI_APPLICATION = 'powerhaus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
