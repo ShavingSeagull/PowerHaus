89ac6c646ca067320169b197eda88a3c84927f16
Scott Kipp <scott.ecfx@gmail.com>
2021-04-06 18:08:33 +0100

    Changed secret key and added env variable to settings

--- powerhaus/settings.py
+++ powerhaus/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&b4goohc!&=j2_0h*$+7sn5)lvx$g=+sdxl-y#4d&$i!uhk47s'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
