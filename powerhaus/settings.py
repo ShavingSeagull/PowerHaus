3221255fd2f17bdbefd1742d4cb83afa3306e5fb
Scott Kipp <scott.ecfx@gmail.com>
2021-04-06 12:32:59 +0100

    Added previously made index and about pages to home app

--- powerhaus/settings.py
+++ powerhaus/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&b4goohc!&=j2_0h*$+7sn5)lvx$g=+sdxl-y#4d&$i!uhk47s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
