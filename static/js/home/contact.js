61cf4614427b67f1cfb74d987de514a4712dc4c0
Scott Kipp <scott.ecfx@gmail.com>
2021-04-08 16:23:21 +0100

    Streamlined the static directory tree to include each app

--- static/js/home/contact.js
+++ static/js/home/contact.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Fires the confirmation modal if the hidden variable in the template is True
-    if ($('#fire_modal').val().toBool() == "True") {
+    if ($('#fire_modal').val() == "True") {
         $('#messageModal').modal();
     }
 });
