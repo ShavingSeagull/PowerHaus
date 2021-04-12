a61c446f1f8fcb83cfd91810b7837cd964684732
Scott Kipp <scott.ecfx@gmail.com>
2021-04-12 15:57:06 +0100

    Added if/else to show profile URL on home page

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def view_profile(request):
+    """
+    Function to render the profile page. Requires login.
+    """
     return render(request, "profiles/profile_base.html")
