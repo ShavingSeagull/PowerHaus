7700cc3f4df46016c795fb53205af3a5c6d384b8
Scott Kipp <scott.ecfx@gmail.com>
2021-04-15 20:22:17 +0100

    Added URL filtering to products view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.urls import resolve
+from .models import Product
 
 def products(request):
-    return render(request, "products/products_list.html")
+    """
+    Uses the url to filter the products shown by category
+    """
+    current_url = resolve(request.path_info).url_name
+    products = Product.objects.filter(category__name=current_url).order_by('name')
+    context = {
+        "products": products
+    }
+    return render(request, "products/products_list.html", context=context)
---
+++ static/js/products/sticky_nav.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // Code inspired by W3Schools:
+    // https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+
+    let nav = document.getElementById("product-nav");
+    let sticky = nav.offsetTop;
+
+    function skinnyNav() {
+        if (window.pageYOffset >= sticky) {
+            nav.classList.add("sticky-nav")
+        } else {
+            nav.classList.remove("sticky-nav");
+        }
+    }
+
+    window.onscroll = function() {
+        skinnyNav();
+    };
+});
