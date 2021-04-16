9403cc2932fff41b4b946062ddcf97981154a2c0
Scott Kipp <scott.ecfx@gmail.com>
2021-04-16 12:28:36 +0100

    Added search functionality to products page

--- products/views.py
+++ products/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import resolve
+from django.db.models import Q
 from .models import Product
 
 def products(request):
     """
-    Uses the url to filter the products shown by category
+    Displays the product list. Checks if the user entered a search term and, if so, 
+    shows the search results. If not, it will render the products that match the URL 
+    that fired the function as these match the product category names.
     """
     current_url = resolve(request.path_info).url_name
     products = Product.objects.filter(category__name=current_url).order_by('name')
+    
+    if request.GET:
+        if 'q' in request.GET:
+            search_query = request.GET['q']
+            queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
+            products = Product.objects.filter(queries).order_by('name')
+    
     context = {
         "products": products
     }
