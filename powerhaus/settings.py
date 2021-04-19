349552a9da8caedfc0e96851669a6255e7aa3581
Scott Kipp <scott.ecfx@gmail.com>
2021-04-19 17:23:27 +0100

    Added Promocodes app and context file

--- powerhaus/settings.py
+++ powerhaus/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'profiles',
     'products',
     'cart',
+    'promocodes',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
