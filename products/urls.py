ad040203771a2f1c63e09ed8e6f69150aee556d6
Scott Kipp <scott.ecfx@gmail.com>
2021-04-19 15:32:55 +0100

    Added product detail page and hooked up all the product urls

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import products
+from .views import products, product_item
 
 urlpatterns = [
     path('protein', products, name="protein"),
+    path('vitamins', products, name="vitamins"),
+    path('supplements', products, name="supplements"),
+    path('equipment', products, name="equipment"),
+    path('<category>/<product_id>', product_item, name="product_item"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import resolve
 from django.db.models import Q
 from .models import Product
@@ -17,8 +17,23 @@ def products(request):
             search_query = request.GET['q']
             queries = Q(name__icontains=search_query) | Q(description__icontains=search_query)
             products = Product.objects.filter(queries).order_by('name')
+            current_url = None
     
     context = {
-        "products": products
+        "products": products,
+        "url": current_url
     }
     return render(request, "products/products_list.html", context=context)
+
+def product_item(request, category, product_id):
+    """
+    Returns the selected item from the product list and displays its 
+    details. Uses the product's category to maintain the appropriate 
+    header image.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        "product": product,
+        "url": category
+    }
+    return render(request, "products/products_item.html", context=context)
