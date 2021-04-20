cdc08eecbbcf98aa6188e07b58c5bf4504991a20
Scott Kipp <scott.ecfx@gmail.com>
2021-04-20 12:58:22 +0100

    Fixed quantity update method in cart for Jinja made forms

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import view_cart, add_to_cart, adjust_cart, remove_from_cart
 
 urlpatterns = [
-    path('', view_cart, name="cart"),
+    path('your-cart', view_cart, name="cart"),
     path('add-to-cart/<id>', add_to_cart, name="add_to_cart"),
     path('adjust-cart/<id>', adjust_cart, name="adjust_cart"),
     path('remove-from-cart/<id>', remove_from_cart, name="remove_from_cart")
--- static/js/cart/cart.js
+++ static/js/cart/cart.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $("#cart-submit-btn").click(() => {
-        $("#cart-quantity").submit();
+    // Click listener submits the form when the submit button is clicked. 
+    // Needed due to the submit button not being inside the quantity form.
+    // DOM traversal needed as forms are rendered in a Jinja loop.
+    $(".cart-submit-btn").click(function(e) {
+        let form = $(this).parent().prev('.quantity-form');
+        form.submit();
     });
 });
