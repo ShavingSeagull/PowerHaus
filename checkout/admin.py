533638293e33afc103412600f833d4cab77c3168
Scott Kipp <scott.ecfx@gmail.com>
2021-04-26 11:58:56 +0100

    Added final URLs, admin and apps setup for checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                        'subtotal', 'total', 
+                        'original_cart', 'stripe_pid')
+
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
+              'email', 'phone', 'country',
+              'post_code', 'city', 'address_1',
+              'address_2', 'county', 'subtotal', 'total', 
+              'original_cart', 'stripe_pid')
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'subtotal', 'total')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
