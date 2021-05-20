5956ce629654859410910941237cf231daa84c01
Scott Kipp <scott.ecfx@gmail.com>
2021-05-20 12:14:54 +0100

    Added Heroku URL to allowed hosts

--- powerhaus/settings.py
+++ powerhaus/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'powerhaus.herokuapp.com']
 
 
 # Application definition
