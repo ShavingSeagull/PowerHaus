6620aefd6afe120f11860af041982cedf4ac0f47
Scott Kipp <scott.ecfx@gmail.com>
2021-05-20 11:59:11 +0100

    Added setup for Postgres to settings

--- powerhaus/settings.py
+++ powerhaus/settings.py
@@ -4,6 +4,7 @@ Django settings for powerhaus project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -104,12 +105,17 @@ WSGI_APPLICATION = 'powerhaus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if "DEV" in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("POSTGRES_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
